f2e68f16fc96d702b9b4a298692152a5b3f17229
yassin-ben-haj-ali <chaima.techini@sastec-group.com>
2024-12-24 15:36:43 +0100

    implement request body validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authControllers from "../controllers/auth.js";
 import catchMiddleware from "../middlewares/api.js"
+import validate from "../middlewares/validate.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", catchMiddleware(authControllers.register));
+authRouter.post("/register", validate("register"),catchMiddleware(authControllers.register));
 authRouter.post("/login", catchMiddleware(authControllers.login));
 authRouter.get("/refresh", catchMiddleware(authControllers.refreshToken));
 
